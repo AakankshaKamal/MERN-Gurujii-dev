ae1a30a330d1e0f6b585cd51a4eb2320ac051348
AakankshaKamal <aakankshakamal.post@gmail.com>
2021-04-09 21:41:54 +0530

    Temp saving

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Home from './components/home';
@@ -7,29 +7,55 @@ import './App.css';
 import Board from './board.jpg';
 import Footer from './components/Footer';
 
+import ClipLoader from 'react-spinners/ClockLoader';
 function App() {
+  const [loading, setloading] = useState(false);
+  useEffect(() => {
+    setloading(true);
+    setTimeout(() => {
+      setloading(false);
+    }, 3000);
+  }, []);
+
   return (
     <>
-    <Router>
-      <div className='parallax'>
-        <Navbar />
-        <br />
-        <br />
-        <br />
-        <div className='center'>
-          <img src={Board} alt='' />
+      {loading ? (
+        <div className='splash'>
+          <div className='logo'>
+            <ClipLoader color={' #ffb3b3'} loading={loading} size={200} />
+          </div>
+          <div className='splash-screen'>
+            Wait a moment while we launch the website:
+            <b> GURUJI</b>
+            <div className='loading-dot'>.</div>
+          </div>
         </div>
-        <br />
-        <br />
-        <br />
-        <br />
-      </div>
+      ) : (
+        <>
+          <Router>
+            <div className='parallax'>
+              <Navbar />
+              <br />
+              <br />
+              <br />
+              <div className='center'>
+                <img src={Board} alt='' />
+              </div>
+              <br />
+              <br />
+              <br />
+              <br />
+            </div>
 
-      <br />
-      <Route path='/' exact component={Home} />
-      <ChatBotComponent />
-    </Router>
-    <Footer/>
+            <br />
+
+            <Route path='/' exact component={Home} />
+            <ChatBotComponent />
+          </Router>
+
+          <Footer />
+        </>
+      )}
     </>
   );
 }
