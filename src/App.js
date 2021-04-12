3209dfdd165f11c58cd4acd4064461a4ef66259d
AakankshaKamal <aakankshakamal.post@gmail.com>
2021-04-13 00:00:53 +0530

    Light background

--- src/App.js
+++ src/App.js
@@ -8,13 +8,14 @@ import Board from './board.jpg';
 import Footer from './components/Footer';
 
 import ClipLoader from 'react-spinners/ClockLoader';
+import SkewLoader from 'react-spinners/BounceLoader';
 function App() {
   const [loading, setloading] = useState(false);
   useEffect(() => {
     setloading(true);
     setTimeout(() => {
       setloading(false);
-    }, 3000);
+    }, 5000);
   }, []);
 
   return (
@@ -24,10 +25,14 @@ function App() {
           <div className='logo'>
             <ClipLoader color={' #ffb3b3'} loading={loading} size={200} />
           </div>
+
           <div className='splash-screen'>
             Wait a moment while we launch the website:
             <b> GURUJI</b>
             <div className='loading-dot'>.</div>
+            <div className='logo2'>
+              <ClipLoader color={' #ffb3b3'} loading={loading} size={200} />
+            </div>
           </div>
         </div>
       ) : (
